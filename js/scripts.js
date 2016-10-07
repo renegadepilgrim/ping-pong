8b7dd2ba9b01ac659caf3d5cd56ee6720e0dade8
Heather Knight <heather.knight.pdx@gmail.com>
2016-10-06 17:00:37 -0700

    set up intial index.html, css, js, and img files for project

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//(user interface)front-end logic
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+
+
+
+
+    $("result").text(result);
+  });
+});
+
+
+
+//(business)back-end logic
+var ping-pong = function(number)  {
+  if (integer % 3 ===)
+}
