367cc2385e089830d46e13d46b704a258795afb0
Heather Knight <heather.knight.pdx@gmail.com>
2016-10-07 17:12:41 -0700

    update jquery and javascript with front and back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
 //(user interface)front-end logic
 $(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
+  $("form#pingpong").submit(function(event) {
     event.preventDefault();
+    var userInput = $("input#userInput").val();
+    var result = pingpong(userInput);
 
-
-
-
-    $("result").text(result);
+    $("#result").text(result);
+    $("#result").show();
   });
 });
 
 
 
 //(business)back-end logic
-var ping-pong = function(number)  {
-  if (integer % 3 ===)
-}
+var pingpong = function(number)  {
+  if (number % 3 === 0 && number % 5 === 0) {
+    output.push("pingpong");
+  } else if (number % 3 === 0) {
+    output.push("ping");
+  } else if (number % 5 === 0) {
+    output.push("pong");
+  } else {
+    output.push(number);
+  });
+});
