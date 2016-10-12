ec5d26dcb990ad5a395501ed607ad4e770d5f1ea
Heather Knight <heather.knight.pdx@gmail.com>
2016-10-12 16:17:17 -0700

    update js and css code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,38 @@
+//(business)back-end logic
+pingpong = function(userNumber)  {
+
+  var result = [];
+
+  for (var number =1; number <= userNumber; number++) {
+    if (number % 15 === 0) {
+      result.push("pingpong");
+    //$("#result").append("ping pong");
+    } else if (number % 3 === 0) {
+      result.push("pong");
+  //  $("#result").append("ping");
+    } else if (number % 5 === 0) {
+      result.push("ping");
+  //  $("#result").append("pong");
+    } else {
+      result.push(number);
+  //  $("#result").append(number);
+    }
+  }
+  return result.join(", ");
+};
+
+
 //(user interface)front-end logic
 $(document).ready(function() {
   $("form#pingpong").submit(function(event) {
     event.preventDefault();
-    var userInput = $("input#userInput").val();
+
+  //$("#result").empty();
+
+    var userInput = $("input#userValue").val();
     var result = pingpong(userInput);
 
     $("#result").text(result);
     $("#result").show();
-  });
-});
-
-
-
-//(business)back-end logic
-var pingpong = function(number)  {
-  if (number % 3 === 0 && number % 5 === 0) {
-    output.push("pingpong");
-  } else if (number % 3 === 0) {
-    output.push("ping");
-  } else if (number % 5 === 0) {
-    output.push("pong");
-  } else {
-    output.push(number);
   });
 });
